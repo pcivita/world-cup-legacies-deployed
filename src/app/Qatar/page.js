e27b8b1893241bf7afe3dfe0f92fd4c580db671c
Ayesha Khawaja <ayesha@recidiviz.org>
2024-02-21 14:32:57 -0800

    Broken image comparison slider for Qatar page

--- src/app/Qatar/page.js
+++ src/app/Qatar/page.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Slider } from "../../../components/Slider";
+import sad_messi from './images/sad_messi.jpeg';
+import happy_messi from './images/happy_messi.jpeg';
 
 export default function Page() {
   return (
     <div>
       <h1>I am in Qatar Update</h1>
+      <div>
+        <Slider beforeImage={sad_messi} afterImage={happy_messi} />
+      </div>
     </div>
   );
 }
