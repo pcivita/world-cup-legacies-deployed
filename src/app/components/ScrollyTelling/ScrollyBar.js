c56d7c3006c5dfabff13ae74b76609e2d99075d1
pcivita <pcivita@stanford.edu>
2024-03-06 10:33:16 -0800

    Aesthetic Updates to BarScrolly

--- src/app/components/ScrollyTelling/ScrollyBar.js
+++ src/app/components/ScrollyTelling/ScrollyBar.js
@@ -14,9 +14,9 @@ const ScrollyBar = () => {
   };
 
   return (
-    <div className="relative max-w-3xl mx-auto">
-      <div className="sticky top-1/3 z-0 mb-96 border-2 text-center rounded-lg">
-        <BarGraph currentIndex={currentStepIndex} />
+    <div className="relative mx-20">
+      <div className="w-fit sticky top-1/3 left-1/2 z-0 text-center rounded-lg mx-10">
+        <BarGraph currentIndex={currentStepIndex} className="" />
 
         <p>
           I&apos;m sticky. The current triggered step index is:{" "}
@@ -24,42 +24,55 @@ const ScrollyBar = () => {
         </p>
       </div>
 
-      <Scrollama offset={0.5} onStepEnter={onStepEnter}>
+      <Scrollama
+        offset={0.5}
+        onStepEnter={onStepEnter}
+        className="border-2 z-10"
+      >
         <Step data={0} key={0}>
-          <div className=" w-1/4 h-40 relative bg-gray-200 align-center mx-auto mb-96 shadow-md rounded-lg">
-            <p className="p-8">
-              Over 1 billion reais (~$300 million dollars) of debt remain. Index{" "}
-              {0}{" "}
-            </p>
+          <div className="border-2 w-1/2 z-10 flex justify-center">
+            <div className="w-1/2 h-40 bg-gray-200 mb-96 shadow-md rounded-lg text-center flex items-center justify-center">
+              <p className="p-8 border-2 border-black ">
+                Over 1 billion reais (~$300 million dollars) of debt remain.
+              </p>
+            </div>
           </div>
         </Step>
         <Step data={1} key={1}>
-          <div className=" w-1/4 h-40 relative bg-gray-200 align-center mx-auto mb-96 shadow-md rounded-lg">
-            <p className="p-8">
-              Hello, I would be the second card: Step of index {1}{" "}
-            </p>
+          <div className="border-2 w-1/2 flex justify-center">
+            <div className="w-1/2 h-40 bg-gray-200 mb-96 shadow-md rounded-lg text-center flex items-center justify-center">
+              <p className="p-8 border-2 border-black ">
+                Hello, I would be the second card: Step of index {1}{" "}
+              </p>
+            </div>
           </div>
         </Step>
         <Step data={2} key={2}>
-          <div className=" w-1/4 h-40 relative bg-gray-200 justify-center mx-auto mb-96 shadow-md rounded-lg">
-            <p className="p-8">
-              Hello, I would be the third card: Step of index {2}{" "}
-            </p>
+          <div className="border-2 w-1/2 z-10 flex justify-center">
+            <div className="w-1/2 h-40 bg-gray-200 mb-96 shadow-md rounded-lg text-center flex items-center justify-center">
+              <p className="p-8 border-2 border-black ">
+                Over 1 billion reais (~$300 million dollars) of debt remain.
+              </p>
+            </div>
           </div>
         </Step>
 
         <Step data={3} key={4}>
-          <div className=" w-1/4 h-40 relative bg-gray-200 align-center mx-auto mb-96 shadow-md rounded-lg">
-            <button className="w-full h-full bg-blue-400 rounded-lg">
-              PRESS ME
-            </button>
+          <div className="border-2 w-1/2 z-10 flex justify-center">
+            <div className="w-1/2 h-40 bg-gray-200 mb-96 shadow-md rounded-lg text-center flex items-center justify-center">
+              <p className="p-8 border-2 border-black ">
+                Over 1 billion reais (~$300 million dollars) of debt remain.
+              </p>
+            </div>
           </div>
         </Step>
         <Step data={3} key={4}>
-          <div className=" w-1/4 h-40 relative bg-gray-200 align-center mx-auto mb-96 shadow-md rounded-lg">
-            <button className="w-full h-full bg-blue-400 rounded-lg">
-              Filler
-            </button>
+          <div className="border-2 w-1/2 z-10 flex justify-center">
+            <div className="w-1/2 h-40 bg-gray-200 mb-96 shadow-md rounded-lg text-center flex items-center justify-center">
+              <p className="p-8 border-2 border-black ">
+                Over 1 billion reais (~$300 million dollars) of debt remain.
+              </p>
+            </div>
           </div>
         </Step>
       </Scrollama>
